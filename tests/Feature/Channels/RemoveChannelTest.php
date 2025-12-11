0cf3839420c15c18b2b332140451c3f5db9f0597
kyher <kyle.heron@pm.me>
2025-12-11 14:22:27 +0000

    Adds tests / fix  namespaces

--- tests/Feature/Channels/RemoveChannelTest.php
+++ tests/Feature/Channels/RemoveChannelTest.php
@@ -1,59 +1,54 @@
 <?php
 
-namespace Tests\Feature\Workspaces;
+namespace Tests\Feature\Channels;
 
 use App\Enum\WorkspaceUserRole;
 use App\Models\User;
 use Carbon\Carbon;
+use Database\Factories\ChannelFactory;
 use Database\Factories\WorkspaceFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Inertia\Testing\AssertableInertia as Assert;
 
-class RemoveWorkspaceTest extends TestCase
+class RemoveChannelTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_workspace_can_be_removed()
+    public function test_channel_can_be_removed()
     {
-        Carbon::setTestNow();
         $workspaceOwner = User::factory()->create();
         $workspace = WorkspaceFactory::new()->ownedBy($workspaceOwner)->create();
+        $channel = ChannelFactory::new()->for($workspace)->ownedBy($workspaceOwner)->create();
 
         $response = $this
             ->actingAs($workspaceOwner)
             ->followingRedirects()
-            ->delete(route('workspace.destroy', ['workspace' => $workspace->id]));
+            ->delete(route('channel.destroy', ['channel' => $channel->id]));
 
         $response->assertStatus(200);
-        $this->assertDatabaseHas('workspaces', [
-            'id' => $workspace->id,
-            'deleted_at' => now()
-        ]);
-        $this->assertDatabaseHas('workspace_users', [
-            'user_id' => $workspaceOwner->id,
-            'workspace_id' => $workspace->id,
-            'role' => WorkspaceUserRole::Administrator,
-            'deleted_at' => now()
+        $this->assertDatabaseMissing('channels', [
+            'id' => $channel->id,
         ]);
     }
 
-    public function test_workspace_cannot_be_removed_by_non_owner()
+    public function test_channel_cannot_be_removed_by_non_owner()
     {
         $workspaceOwner = User::factory()->create();
         $workspace = WorkspaceFactory::new()->ownedBy($workspaceOwner)->create();
+        $channel = ChannelFactory::new()->for($workspace)->ownedBy($workspaceOwner)->create();
 
         $otherUser = User::factory()->create();
 
         $response = $this
             ->actingAs($otherUser)
             ->followingRedirects()
-            ->delete(route('workspace.destroy', ['workspace' => $workspace->id]));
+            ->delete(route('channel.destroy', ['channel' => $channel->id]));
 
         $response->assertInertia(
             fn(Assert $page) => $page
                 ->has('errors')
-                ->where('errors.0', 'Could not delete workspace')
+                ->where('errors.0', 'You do not have permission to delete this channel')
         );
     }
 }
--- tests/Feature/Workspaces/StoreWorkspaceTest.php
+++ tests/Feature/Workspaces/StoreWorkspaceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Settings;
+namespace Tests\Feature\Workspaces;
 
 use App\Enum\WorkspaceUserRole;
 use App\Models\User;
